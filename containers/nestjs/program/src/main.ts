a44f01831764e4d6f95f0c3c3857f4e2bacd2588
MyNameIsTrez <welfje@gmail.com>
2024-01-30 13:36:37 +0100

    Print `foo` every second

--- containers/nestjs/program/src/main.ts
+++ containers/nestjs/program/src/main.ts
@@ -4,5 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4242);
+
+  const interval_ms = 1000;
+  setInterval(() => console.log('foo'), interval_ms);
 }
 bootstrap();
