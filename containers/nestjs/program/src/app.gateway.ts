8dcea4a245ba0c0c21b827fe6e1e1c3e4fec861d
MyNameIsTrez <welfje@gmail.com>
2024-01-30 13:37:21 +0100

    Add handleConnection() and handleDisconnect()

--- containers/nestjs/program/src/app.gateway.ts
+++ containers/nestjs/program/src/app.gateway.ts
@@ -3,15 +3,40 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { Server } from 'http';
 
-// cors setting prevents this error:
+// The cors setting prevents this error:
 // "Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource"
 @WebSocketGateway({ cors: { origin: '*' } })
-export class AppGateway {
+export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
+
+  clients = new Set<Socket>();
+
+  handleConnection(client: Socket) {
+    this.clients.add(client);
+    this.broadcast('events', 'A player connected');
+    console.log('Client connected');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.clients.delete(client);
+    this.broadcast('events', 'A player disconnected');
+    console.log('Client disconnected');
+  }
+
+  private broadcast(event: string, message: string) {
+    this.clients.forEach((client) => {
+      client.emit(event, message);
+      // console.log(client);
+      console.log('Sent message to client');
+    });
+  }
 
   @SubscribeMessage('identity')
   async identity(@MessageBody() data: number): Promise<number> {
