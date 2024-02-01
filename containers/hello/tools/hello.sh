a2d4cca15eb8637e08eea757d0933f3dea9401f9
MyNameIsTrez <welfje@gmail.com>
2024-02-01 16:36:13 +0100

    Split into nestjs and vuejs containers

--- containers/hello/tools/hello.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This lets the script exit if any command fails
-set -e
-
-echo hello
--- containers/vuejs/vite.config.ts
+++ containers/vuejs/vite.config.ts
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 2424,
+    host: true
+  },
   plugins: [
     vue(),
   ],
