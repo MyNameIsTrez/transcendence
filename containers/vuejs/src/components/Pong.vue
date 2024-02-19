d176418e2346417c162533a07f45c96b94b35623
Saladin Afoh <safoh@student.codam.nl>
2024-02-19 17:55:09 +0100

    Rewritten to composition API

--- containers/vuejs/src/components/Pong.vue
+++ containers/vuejs/src/components/Pong.vue
@@ -1,77 +1,70 @@
 <template>
   <div>
-    <canvas ref="pongCanvas"></canvas>
+    <canvas ref="canvasRef"></canvas>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import ScoreBoard from './ScoreBoard.vue'
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import io from 'socket.io-client'
 
 const WINDOW_WIDTH = 1920
 const WINDOW_HEIGHT = 1080
+const socket = io('ws://localhost:4242')
+const canvasRef = ref(null)
+let _context = ref(null)
 
-export default defineComponent({
-  components: {
-    ScoreBoard
-  },
-  mounted() {
-    this.socket = io('ws://localhost:4242')
-    this.socket.on('pong', (data) => {
-      this.render(data)
-    })
-    this.initCanvas()
-  },
-  methods: {
-    initCanvas() {
-      const canvas = this.$refs.pongCanvas as HTMLCanvasElement
-      canvas.tabIndex = 1 // make element focusable, so that addEventListener can be used
-      canvas.addEventListener('keydown', (event) => {
-        this.socket.emit('pressed', event.code)
-      })
-      canvas.addEventListener('keyup', (event) => {
-        this.socket.emit('released', event.code)
-      })
-      canvas.width = WINDOW_WIDTH
-      canvas.height = WINDOW_HEIGHT
-      const context = canvas.getContext('2d')
-      if (context) {
-        this._context = context
-        this.drawCanvas()
-      }
-    },
-    drawObject(
-      color: string,
-      obj: { pos: { x: number; y: number }; size: { w: number; h: number } }
-    ) {
-      if (this._context) {
-        this._context.fillStyle = color
-        this._context.fillRect(obj.pos.x, obj.pos.y, obj.size.w, obj.size.h)
-      }
-    },
-    drawCanvas() {
-      if (this._context) {
-        this._context.fillStyle = 'black'
-        this._context.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
-      }
-    },
-    render(data: {
-      ball: any
-      leftPlayer: { paddle: any; score: number }
-      rightPlayer: { paddle: any; score: number }
-    }) {
-      this.drawCanvas()
-      this.drawObject('white', data.ball)
-      this.drawObject('white', data.leftPlayer.paddle)
-      this.drawObject('white', data.rightPlayer.paddle)
-      // this.$refs.scoreBoard.updateScore(data.leftPlayer.score, data.rightPlayer.score);
-    },
-    start() { }
-  }
+onMounted(() => {
+  socket.on('pong', (data) => {
+    render(data)
+    console.log('received pong:', data)
+  })
+  initCanvas()
 })
+const initCanvas = () => {
+  const canvas = canvasRef.value
+  canvas.tabIndex = 1 // make element focusable, so that addEventListener can be used
+  canvas.addEventListener('keydown', (event) => {
+    socket.emit('pressed', event.code)
+  })
+  canvas.addEventListener('keyup', (event) => {
+    socket.emit('released', event.code)
+  })
+  canvas.width = WINDOW_WIDTH
+  canvas.height = WINDOW_HEIGHT
+  const context = canvas.getContext('2d')
+  if (context) {
+    _context.value = context
+    drawCanvas()
+  }
+}
+const drawObject = (
+  color: string,
+  obj: { pos: { x: number; y: number }; size: { w: number; h: number } }
+) => {
+  if (_context.value) {
+    _context.value.fillStyle = color
+    _context.value.fillRect(obj.pos.x, obj.pos.y, obj.size.w, obj.size.h)
+  }
+}
+const drawCanvas = () => {
+  if (_context.value) {
+    _context.value.fillStyle = 'black'
+    _context.value.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
+  }
+}
+const render = (data: {
+  ball: any
+  leftPlayer: { paddle: any; score: number }
+  rightPlayer: { paddle: any; score: number }
+}) => {
+  drawCanvas()
+  drawObject('white', data.ball)
+  drawObject('white', data.leftPlayer.paddle)
+  drawObject('white', data.rightPlayer.paddle)
+  // this.$refs.scoreBoard.updateScore(data.leftPlayer.score, data.rightPlayer.score);
+}
+// const start = () =>{};
 </script>
 
-<style>
-/* Add styles if needed */
-</style>
+<style></style>
