7bbced8663f7e4c7ce53218c85a5da80b0f9e483
MyNameIsTrez <welfje@gmail.com>
2024-02-19 20:01:15 +0100

    Simplify Dockerfiles while fixing prettier

--- containers/nestjs/src/app.gateway.ts
+++ containers/nestjs/src/app.gateway.ts
@@ -6,42 +6,40 @@ import {
   OnGatewayInit,
   OnGatewayConnection,
   OnGatewayDisconnect,
-  ConnectedSocket,
-} from '@nestjs/websockets';
-import { Socket } from 'socket.io';
-import { Server } from 'http';
-import { Pong } from './pong';
+  ConnectedSocket
+} from '@nestjs/websockets'
+import { Socket } from 'socket.io'
+import { Server } from 'http'
+import { Pong } from './pong'
 
 // The cors setting prevents this error:
 // "Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource"
 @WebSocketGateway({ cors: { origin: '*' } })
-export class AppGateway
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
-{
+export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
-  server!: Server;
+  server!: Server
 
-  pong = new Pong();
+  pong = new Pong()
 
-  player1: Socket | null = null;
-  player2: Socket | null = null;
+  player1: Socket | null = null
+  player2: Socket | null = null
 
   handleConnection(client: Socket) {
-    console.log('Handle connection called');
+    console.log('Handle connection called')
     if (this.player1 == null) {
-      this.player1 = client;
-      console.log('Player 1 joined');
+      this.player1 = client
+      console.log('Player 1 joined')
       if (this.player2 != null) {
-        this.player2.emit('opponentDisconnected', 0);
+        this.player2.emit('opponentDisconnected', 0)
       }
     } else if (this.player2 == null) {
-      this.player2 = client;
-      console.log('Player 2 joined');
+      this.player2 = client
+      console.log('Player 2 joined')
       if (this.player1 != null) {
-        this.player1.emit('opponentDisconnected', 0);
+        this.player1.emit('opponentDisconnected', 0)
       }
     } else {
-      console.log('A third player tried to join!');
+      console.log('A third player tried to join!')
     }
 
     // this.broadcast('events', 'A player connected');
@@ -59,72 +57,66 @@ export class AppGateway
 
   handleDisconnect(client: Socket) {
     if (this.player1 != null && client.id == this.player1.id) {
-      this.player1 = null;
-      console.log(`Player 1 disconnected`);
+      this.player1 = null
+      console.log(`Player 1 disconnected`)
       if (this.player2 != null) {
-        this.player2.emit('opponentDisconnected', 1);
+        this.player2.emit('opponentDisconnected', 1)
       }
     } else if (this.player2 != null && client.id == this.player2.id) {
-      this.player2 = null;
-      console.log(`Player 2 disconnected`);
+      this.player2 = null
+      console.log(`Player 2 disconnected`)
       if (this.player1 != null) {
-        this.player1.emit('opponentDisconnected', 1);
+        this.player1.emit('opponentDisconnected', 1)
       }
     } else {
-      console.log('Another client disconnected');
+      console.log('Another client disconnected')
     }
     if (this.player1 == null && this.player2 == null) {
-      this.pong.resetGame();
-      console.log(`Game has been reset`);
+      this.pong.resetGame()
+      console.log(`Game has been reset`)
     }
   }
 
   afterInit() {
-    const interval_ms = 1000 / 60;
-    this.pong.resetGame();
+    const interval_ms = 1000 / 60
+    this.pong.resetGame()
     setInterval(() => {
       // TODO: I don't think this is a good way to wait for both players to connect, see if there is a better way later. -Victor
       if (this.player1 != null && this.player2 != null) {
-        this.pong.update();
-        const data = this.pong.getData();
-        this.player1?.emit('pong', data);
-        this.player2?.emit('pong', data);
+        this.pong.update()
+        const data = this.pong.getData()
+        this.player1?.emit('pong', data)
+        this.player2?.emit('pong', data)
       }
-    }, interval_ms);
+    }, interval_ms)
   }
 
   @SubscribeMessage('identity')
   async identity(@MessageBody() data: number): Promise<number> {
-    return data;
+    return data
   }
 
   @SubscribeMessage('pressed')
-  async pressed(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() keyName: string,
-  ) {
-    const isPlayer1 = client.id === this.player1?.id;
-    const isPlayer2 = client.id === this.player2?.id;
+  async pressed(@ConnectedSocket() client: Socket, @MessageBody() keyName: string) {
+    const isPlayer1 = client.id === this.player1?.id
+    const isPlayer2 = client.id === this.player2?.id
 
     if (isPlayer1) {
-      this.pong.clientPressedKey(keyName, this.pong._leftPlayer);
+      this.pong.clientPressedKey(keyName, this.pong._leftPlayer)
     } else if (isPlayer2) {
-      this.pong.clientPressedKey(keyName, this.pong._rightPlayer);
+      this.pong.clientPressedKey(keyName, this.pong._rightPlayer)
     }
   }
 
   @SubscribeMessage('released')
-  async released(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() keyName: string,
-  ) {
-    const isPlayer1 = client.id === this.player1?.id;
-    const isPlayer2 = client.id === this.player2?.id;
+  async released(@ConnectedSocket() client: Socket, @MessageBody() keyName: string) {
+    const isPlayer1 = client.id === this.player1?.id
+    const isPlayer2 = client.id === this.player2?.id
 
     if (isPlayer1) {
-      this.pong.clientReleasedKey(keyName, this.pong._leftPlayer);
+      this.pong.clientReleasedKey(keyName, this.pong._leftPlayer)
     } else if (isPlayer2) {
-      this.pong.clientReleasedKey(keyName, this.pong._rightPlayer);
+      this.pong.clientReleasedKey(keyName, this.pong._rightPlayer)
     }
   }
 }
--- containers/nestjs/src/main.ts
+++ containers/nestjs/src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {});
+  const app = await NestFactory.create(AppModule, {})
 
   // TODO: What is this for? I got it from here: https://medium.com/js-dojo/how-to-serve-vue-with-nest-f23f10b33e1
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api')
 
-  await app.listen(4242);
+  await app.listen(4242)
 }
-bootstrap();
+bootstrap()
