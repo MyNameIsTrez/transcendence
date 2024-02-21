14a6e860aac93e340fbe82330d8ecc2cad908fbb
Saladin Afoh <safoh@student.codam.nl>
2024-02-21 20:00:13 +0100

    Readded window reszing

--- containers/vuejs/src/components/Pong.vue
+++ containers/vuejs/src/components/Pong.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="pong-container">
-    <canvas ref="canvasRef"></canvas>
-    <div class="start-button-container">
-      <button class="start-button" @click="connect">PLAY</button>
-    </div>
+  <canvas ref="canvasRef"></canvas>
+  <div class="play-button-container">
+    <button class="play-button" @click="connect">PLAY</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue'
 import { setupGameSocket } from './GameSocket.ts'
-
-const WINDOW_WIDTH = 1920
-const WINDOW_HEIGHT = 1080
 const canvasRef = ref(null)
 
 const render = (data: {
@@ -24,8 +19,8 @@ const render = (data: {
   drawObject('white', data.ball)
   drawObject('white', data.leftPlayer.paddle)
   drawObject('white', data.rightPlayer.paddle)
-  // this.$refs.scoreBoard.updateScore(data.leftPlayer.score, data.rightPlayer.score);
 }
+
 const { connect, disconnect, emitMovePaddle } = setupGameSocket(render)
 
 onMounted(() => {
@@ -36,6 +31,7 @@ onUnmounted(() => {
 })
 function initCanvas() {
   const canvas = canvasRef.value
+  window.addEventListener('resize', drawCanvas) //TODO: replace with render to redraw all objects with correct size
   canvas.tabIndex = 1 // make element focusable, so that addEventListener can be used
   canvas.addEventListener('keydown', (event) => {
     emitMovePaddle(event.code, true)
@@ -43,9 +39,29 @@ function initCanvas() {
   canvas.addEventListener('keyup', (event) => {
     emitMovePaddle(event.code, false)
   })
-  canvas.width = WINDOW_WIDTH
-  canvas.height = WINDOW_HEIGHT
   drawCanvas()
+}
+const resizeCanvas = (canvas) => {
+  const server_window_height = 1080
+  const server_window_width = 1920
+
+  const aspectRatio = server_window_width / server_window_height
+
+  const screenWidth = window.innerWidth
+  const screenHeight = window.innerHeight
+
+  let canvasWidth, canvasHeight
+
+  if (screenWidth / screenHeight > aspectRatio) {
+    canvasWidth = screenHeight * aspectRatio
+    canvasHeight = screenHeight
+  } else {
+    canvasWidth = screenWidth
+    canvasHeight = screenWidth / aspectRatio
+  }
+
+  canvas.width = canvasWidth
+  canvas.height = canvasHeight
 }
 
 function drawObject(
@@ -62,33 +78,89 @@ function drawObject(
 
 function drawCanvas() {
   const canvas = canvasRef.value
+  resizeCanvas(canvas)
   const context = canvas.getContext('2d')
   if (context) {
     context.fillStyle = 'black'
-    context.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
+    context.fillRect(0, 0, canvas.width, canvas.height)
   }
 }
 </script>
 <style>
 .pong-container {
+  display: flex;
+  margin-right: auto;
+  margin-left: auto;
+  flex-direction: column;
 }
-.start-button-container {
+.play-button-container {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 }
-.start-button {
+.play-button {
   background-color: white;
   color: black;
-  padding: 40px 80px;
+  padding: 32px 64px;
   border: none;
   font-size: 32px;
   font-family: 'Press Start 2P', cursive;
   cursor: pointer;
   transition: background-color 0.3s;
 }
-.start-button:hover {
+.play-button:hover {
   background-color: #333; /* Darker shade on hover */
+}
+
+body {
+  align-content: center;
+  display: flex;
+  margin: 0 auto;
+  overflow: hidden;
+}
+canvas {
+  border-top: 10px solid blue;
+  border-bottom: 10px solid blue;
+  image-rendering: pixelated;
+  background-color: black;
+}
+#opponent-disconnected-message {
+  opacity: 1;
+  background-color: red;
+  border: 3px;
+  padding-left: auto;
+  padding-right: auto;
+  margin: auto;
+  font-size: 20px;
+  padding: 5px;
+  border-style: solid;
+  border-width: 3px;
+}
+#start-button {
+  border: 1;
+  font-size: 60px;
+  background-color: lightskyblue;
+  color: rgb(0, 0, 0);
+  width: auto;
+  margin-right: auto;
+  margin-left: auto;
+}
+#stop-button {
+  border: 1;
+  font-size: 60px;
+  background-color: coral;
+  color: rgb(0, 0, 0);
+  width: auto;
+  margin-right: auto;
+  margin-left: auto;
+}
+.center-button {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 }
 </style>
