394bd1c6344c9f523ee096264951f8d228902dc9
Milan Forsthövel <mforstho@student.codam.nl>
2024-03-04 14:11:57 +0100

    Finished concept for profile page

--- containers/vuejs/src/components/Sidebar.vue
+++ containers/vuejs/src/components/Sidebar.vue
@@ -29,7 +29,73 @@
 				<span class="material-icons">person</span>
 				<span class="text">Profile</span>
 			</router-link>
-			<!-- <router-link to="/about" class="button">
+			<div class="testing">
+				<h1>Your profile</h1>
+				<p>mforstho</p>
+				<div style="clear: both">
+					<img style="float: left" src="/vuejs/code/src/assets/mforstho.png">
+					<p style="float: right">Win/Loss ratio: 121/0</p>
+				</div>
+
+
+
+
+				<div style="clear: both; padding-top: 50px;">
+					<h1 style="padding-bottom: 10px">Match history</h1>
+				</div>
+				<div style="clear: both">
+					<p class="player1" style="float: left; padding-left: 5px; color: rgb(98, 98, 255);">mforstho</p>
+					<p class="player2" style="float: right; padding-right: 5px;">safoh</p>
+					<p class="match-date" style="text-align: center">04/03/2024</p>
+				</div>
+				<p>------------------------------------------------------</p>
+				<div style="clear: both; padding-bottom: 10px;">
+					<p style="float: left; padding-left: 10px; color: var(--primary);">WIN</p>
+					<p style="float: right; padding-right: 10px; color: red;">LOSS</p>
+					<p style="text-align: center">|</p>
+				</div>
+				<div style="clear: both">
+					<p class="player1" style="float: left; padding-left: 5px; color: rgb(98, 98, 255);">mforstho</p>
+					<p class="player2" style="float: right; padding-right: 5px;">safoh</p>
+					<p class="match-date" style="text-align: center">04/03/2024</p>
+				</div>
+				<p>------------------------------------------------------</p>
+				<div style="clear: both; padding-bottom: 10px;">
+					<p style="float: left; padding-left: 10px; color: var(--primary);">WIN</p>
+					<p style="float: right; padding-right: 10px; color: red;">LOSS</p>
+					<p style="text-align: center">|</p>
+				</div>
+				<div style="clear: both">
+					<p class="player1" style="float: left; padding-left: 5px; color: rgb(98, 98, 255);">mforstho</p>
+					<p class="player2" style="float: right; padding-right: 5px;">safoh</p>
+					<p class="match-date" style="text-align: center">04/03/2024</p>
+				</div>
+				<p>------------------------------------------------------</p>
+				<div style="clear: both; padding-bottom: 10px;">
+					<p style="float: left; padding-left: 10px; color: var(--primary);">WIN</p>
+					<p style="float: right; padding-right: 10px; color: red;">LOSS</p>
+					<p style="text-align: center">|</p>
+				</div>
+
+
+
+				<div class="achievements">
+					<h1>Your achievements</h1>
+					<span class="achievement-status">
+						<p>Unlocked</p>
+					</span>
+					<div class="tooltip">A good start<span class="tooltiptext">Win for the first time</span></div>
+					<div class="tooltip">Unstoppable!<span class="tooltiptext">Win 100 times in a row</span></div>
+
+					<span class="achievement-status">
+						<p>Locked</p>
+					</span>
+					<div class="tooltip">Unfortunate<span class="tooltiptext">Lose for the first time</span></div>
+					<div class="tooltip">How!?<span class="tooltiptext">Lose 100 times in a row</span></div>
+				</div>
+			</div>
+		</div>
+		<!-- <router-link to="/about" class="button">
 				<span class="material-icons">group</span>
 				<span class="text">Friends</span>
 			</router-link>
@@ -37,7 +103,7 @@
 				<span class="material-icons">list</span>
 				<span class="text">Leaderboard</span>
 			</router-link> -->
-		</div>
+
 
 		<div class="flex"></div>
 
@@ -76,6 +142,87 @@ aside {
 	padding: 1rem;
 
 	transition: 0.2s ease-in-out;
+
+	.testing {
+		p {
+			margin-left: 20px;
+			margin-right: 20px;
+			text-align: start;
+			font-size: large;
+		}
+
+		img {
+			width: 8rem;
+			margin-left: 20px;
+			display: block;
+			border: 5px solid #ffffff;
+		}
+	}
+
+	.match-report {
+		p {
+			clear: both;
+			margin-left: 20px;
+			margin-right: 20px;
+		}
+
+		.player1 {
+			p {
+				float: left;
+				// text-align: left;
+			}
+		}
+
+		.player2 {
+			p {
+				float: right;
+				// text-align: right;
+			}
+		}
+
+		.match-date {
+			p {
+				float: center;
+			}
+		}
+	}
+
+	.achievements {
+		padding-top: 30px;
+
+		p {
+			padding-left: 10px;
+		}
+
+		.achievement-status {
+			font-weight: bold;
+		}
+	}
+
+	.tooltip {
+		text-align: left;
+		padding-left: 30px;
+		padding-top: 2px;
+		position: relative;
+		// border-bottom: 1px dotted black;
+	}
+
+	.tooltip .tooltiptext {
+		visibility: hidden;
+		width: 120px;
+		background-color: white;
+		color: black;
+		text-align: center;
+		border-radius: 6px;
+		padding: 5px 0;
+
+		position: absolute;
+		z-index: 1;
+	}
+
+	.tooltip:hover .tooltiptext {
+		visibility: visible;
+	}
 
 	.flex {
 		flex: 1 1 0%;
