7f2dd60f9cd0564a2d7fffd2365c669565d3bad7
Victor Bennekom <vbenneko@student.codam.nl>
2024-03-05 16:11:16 +0100

    Get rid of implicit api prefix

--- containers/nestjs/src/main.ts
+++ containers/nestjs/src/main.ts
@@ -5,9 +5,6 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {})
 
-  // TODO: What is this for? I got it from here: https://medium.com/js-dojo/how-to-serve-vue-with-nest-f23f10b33e1
-  app.setGlobalPrefix('api')
-
   const config = app.get(ConfigService)
   const port = config.getOrThrow<number>('NESTJS_PORT')
   await app.listen(port)
