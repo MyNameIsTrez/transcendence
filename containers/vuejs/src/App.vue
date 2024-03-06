ad444627b758b6c6a27e4ad657192dfb0a00ea1a
Saladin Afoh <safoh@student.codam.nl>
2024-03-06 17:34:06 +0100

    Formatting

--- containers/vuejs/src/App.vue
+++ containers/vuejs/src/App.vue
@@ -10,11 +10,9 @@ onBeforeMount(() => {
 onUnmounted(() => {
   disconnectSocketIO('game')
 })
-
 </script>
 
 <template>
-		
   <button class="btn btn-neutral">Neutral</button>
   <Sidebar />
   <PongCanvas />
@@ -35,44 +33,43 @@ body {
   margin: 0 auto;
   font-family: 'Press Start 2P', cursive;
 }
-
 </style>
 <style lang="scss">
 :root {
-	--primary: #4ade80;
-	--primary-alt: #22c55e;
-	--grey: #64748b;
-	--dark: #1e293b;
-	--dark-alt: #334155;
-	--light: #f1f5f9;
-	--sidebar-width: 300px;
+  --primary: #4ade80;
+  --primary-alt: #22c55e;
+  --grey: #64748b;
+  --dark: #1e293b;
+  --dark-alt: #334155;
+  --light: #f1f5f9;
+  --sidebar-width: 300px;
 }
 
 * {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: inherit;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: inherit;
 }
 
 body {
-	background: var(--light);
+  background: var(--light);
 }
 
 button {
-	cursor: pointer;
-	appearance: none;
-	border: none;
-	outline: none;
-	background: none;
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
 }
 
 .app {
-	display: flex;
+  display: flex;
 
-	main {
-		flex: 1 1 0;
-		padding: 2rem;
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
 
     @media (max-width: 1024px) {
       padding-left: 6rem;
