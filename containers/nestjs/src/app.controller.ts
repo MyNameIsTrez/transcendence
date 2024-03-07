b6d82ea02da111e0f95e44f9cdb6a6e18d63ec18
Victor Bennekom <vbenneko@student.codam.nl>
2024-03-07 18:29:33 +0100

    No more implicit `api/` prefix

--- containers/nestjs/src/app.controller.ts
+++ containers/nestjs/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { AppService } from './app.service'
 
-@Controller('api')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   // TODO: Remove
-  @Get('hello')
+  @Get('api/hello')
   getHello(): string {
     return this.appService.getHello()
   }
