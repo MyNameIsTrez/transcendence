6cb39fe9d86a284effa16d3a8734666388c3af0f
Saladin Afoh <safoh@student.codam.nl>
2024-03-08 19:16:51 +0100

    Remove some logging

--- containers/vuejs/src/components/SocketManager.ts
+++ containers/vuejs/src/components/SocketManager.ts
@@ -6,8 +6,6 @@ interface SocketInstance {
 const sockets: SocketInstance = {}
 
 const getSocketIOInstance = (namespace: string): SocketIOClient.Socket | undefined => {
-  console.log('getSocketIOInstance called')
-  console.log('namespace: ', namespace)
   if (!sockets[namespace]) {
     sockets[namespace] = io('ws://localhost:4242')
   }
