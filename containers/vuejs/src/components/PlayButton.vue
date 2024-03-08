56ead6bbdbda27d5dd93c117ae0154ffda92bc6f
Saladin Afoh <safoh@student.codam.nl>
2024-03-08 17:54:45 +0100

    Changed scaling

--- containers/vuejs/src/components/PlayButton.vue
+++ containers/vuejs/src/components/PlayButton.vue
@@ -1,20 +1,10 @@
 <template>
-  <div>
-    <h1 class="title">PONG</h1>
-  </div>
-  <button
-    class="play-button"
-    @click="[joinGame(), changeText()]"
-    :style="{
-      'font-size': fontScale,
-      padding: paddingScale
-    }"
-  >
+  <button class="play-button" @click="[joinGame(), changeText()]">
     {{ displayText }}
   </button>
 </template>
 <script setup lang="ts">
-import { ref, defineProps, computed } from 'vue'
+import { ref } from 'vue'
 import { getSocketIOInstance } from './SocketManager'
 const socketIOGame = getSocketIOInstance('game')
 const joinGame = () => {
@@ -23,41 +13,20 @@ const joinGame = () => {
 }
 const displayText = ref('PLAY')
 
-const props = defineProps({
-  scale: Number
-})
 const changeText = () => {
   displayText.value = 'Seeking match ...'
 }
-
-const fontScale = computed(() => {
-  return `${props.scale * 2}vw`
-})
-const paddingScale = computed(() => {
-  return `${props.scale * 2}vw ${props.scale * 4}vw`
-})
 </script>
 <style>
 .play-button {
   background-color: white;
   color: black;
-  border: none;
-  position: absolute;
-  /*  font-family: inherit;*/
   text-align: center;
-  margin: auto;
-  display: flex;
-  justify-content: center;
+  font-size: 1vw;
+  padding: 1.5vw 3vw;
+  margin: 6vw;
 }
 .play-button:hover {
   background-color: #333; /* Darker shade on hover */
-}
-
-.title {
-  background-color: black;
-  color: white;
-  position: absolute;
-  display: relative;
-  align-content: center;
 }
 </style>
