83386f22917af0150c4d715b23fbf5dca1d46a23
Saladin Afoh <safoh@student.codam.nl>
2024-03-19 17:01:24 +0100

    rewritten scoreboard component

--- containers/vuejs/src/components/ScoreBoard.vue
+++ containers/vuejs/src/components/ScoreBoard.vue
@@ -1,36 +1,40 @@
 <template>
-  <canvas ref="canvasRef"></canvas>
+  <div v-if="startGame" class="score-board">
+    {{ leftPlayerScore }}
+    <span> - </span>
+    {{ rightPlayerScore }}
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import Numbers from './Numbers.vue'
+import { ref } from 'vue'
 import { gameSocket } from './SocketManager'
-
-const canvasRef = ref(null)
-
-let numbersInstance = null
-onMounted(() => {
-  numbersInstance = new Numbers(canvasRef.value.width / 2, 50)
+const leftPlayerScore = ref(0)
+const rightPlayerScore = ref(0)
+const startGame = ref(false)
+gameSocket.on('gameOver', () => {
+  leftPlayerScore.value = 0
+  rightPlayerScore.value = 0
+  startGame.value = false
 })
-
-const updateScore = (leftScore: number, rightScore: number) => {
-  const context = canvasRef.value.getContext('2d')
-  drawScore(context, leftScore, 0)
-  drawScore(context, rightScore, 1)
-}
-
-const drawScore = (context, score: number, i: number) => {
-  const CHAR_W = Numbers.charPixel * 4
-  const align = canvasRef.value.width / 3
-  const chars = score.toString().split('')
-  const offset = align * (i + 1) - CHAR_W * (chars.length / 2) + Numbers.charPixel / 2
-  chars.forEach((char, pos) => {
-    context.drawImage(numbersInstance._numbers[char | 0], offset + pos * CHAR_W, 20)
-  })
-}
-
+gameSocket.on('gameStart', () => {
+  startGame.value = true
+})
 gameSocket.on('pong', (data: any) => {
-  updateScore(data.leftPlayer.score, data.rightPlayer.score)
+  leftPlayerScore.value = data.leftPlayer.score
+  console.log('leftPlayerScore', leftPlayerScore.value)
+  rightPlayerScore.value = data.rightPlayer.score
+  console.log('rightPlayerScore', rightPlayerScore.value)
 })
 </script>
+
+<style>
+.score-board {
+  position: absolute;
+  top: 4vw;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 2vw;
+  color: white;
+}
+</style>
