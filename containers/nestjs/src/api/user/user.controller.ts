d9848459de51ce3d141bc1f5262b9a2369ffc120
MyNameIsTrez <welfje@gmail.com>
2024-04-17 14:20:53 +0200

    Add /setUsername endpoint

--- containers/nestjs/src/api/user/user.controller.ts
+++ containers/nestjs/src/api/user/user.controller.ts
@@ -1,13 +1,21 @@
 import {
+  Body,
   Controller,
   Get,
   Header,
   Param,
+  Post,
   Request,
   StreamableFile,
 } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { createReadStream } from 'fs';
+import { IsNotEmpty } from 'class-validator';
+
+export class SetUsernameDto {
+  @IsNotEmpty()
+  username: string;
+}
 
 @Controller('api/user')
 export class UserController {
@@ -16,6 +24,11 @@ export class UserController {
   @Get('username')
   username(@Request() req) {
     return this.usersService.getUsername(req.user.intra_id);
+  }
+
+  @Post('setUsername')
+  setUsername(@Request() req, @Body() setUsernameDto: SetUsernameDto) {
+    this.usersService.setUsername(req.user.intra_id, setUsernameDto.username);
   }
 
   @Get('intraId')
