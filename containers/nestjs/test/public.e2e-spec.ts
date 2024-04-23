3f1e99f7536ae1cc4e697217f6b563f0b4f33e8f
MyNameIsTrez <welfje@gmail.com>
2024-04-23 16:54:37 +0200

    Add tests

--- containers/nestjs/test/public.e2e-spec.ts
+++ containers/nestjs/test/public.e2e-spec.ts
@@ -31,6 +31,11 @@ describe('App (e2e)', () => {
     await app.init();
   });
 
+  afterEach(async () => {
+    jest.useRealTimers();
+    await app.close();
+  });
+
   it('/api/public/leaderboard (GET)', () => {
     return request(app.getHttpServer())
       .get('/api/public/leaderboard')
@@ -38,31 +43,102 @@ describe('App (e2e)', () => {
       .expect({ sander: 42, victor: 69 });
   });
 
-  it('/api/chat/create (POST)', () => {
+  it('/api/chat/create (POST) - PUBLIC', () => {
+    return api_chat_create('foo', 'PUBLIC', 'foo', '');
+  });
+  it('/api/chat/create (POST) - PROTECTED', () => {
+    return api_chat_create('foo', 'PROTECTED', 'foo', 'foo');
+  });
+  it('/api/chat/create (POST) - PRIVATE', () => {
+    return api_chat_create('foo', 'PRIVATE', 'foo', '');
+  });
+  it('/api/chat/create (POST) - empty name', () => {
+    return api_chat_create('', 'PUBLIC', 'foo', '');
+  });
+  it('/api/chat/create (POST) - unrecognized visibility', () => {
+    return api_chat_create('foo', 'foo', 'foo', '');
+  });
+  it('/api/chat/create (POST) - empty password', () => {
+    return api_chat_create('foo', 'PUBLIC', '', '');
+  });
+  it('/api/chat/create (POST) - unauthorized', () => {
     return request(app.getHttpServer())
       .post('/api/chat/create')
       .send({ name: 'foo', visibility: 'PUBLIC', password: 'foo' })
-      .set('Authorization', bearer_value)
       .set('Content-Type', 'application/json')
+      .expect(500)
+      .expect({ statusCode: 500, message: 'Internal server error' });
+  });
+
+  function api_chat_create(
+    name,
+    visibility,
+    password,
+    expected_hashed_password,
+  ) {
+    return request(app.getHttpServer())
+      .post('/api/chat/create')
+      .send({ name, visibility, password })
+      .set('Content-Type', 'application/json')
+      .set('Authorization', bearer_value)
       .expect(201)
       .then((res) =>
         expect(res.body).toMatchObject({
           chat_id: expect.any(String),
-          name: 'foo',
+          name: name,
           users: [76657],
           history: [],
-          visibility: 'PUBLIC',
-          hashed_password: '',
+          visibility: visibility,
+          hashed_password: expected_hashed_password,
           owner: 76657,
           admins: [76657],
           banned: [],
           muted: [],
         }),
       );
+  }
+
+  it('/api/chat/chats (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/api/chat/chats')
+      .set('Authorization', bearer_value)
+      .expect(200)
+      .expect(['uuid1', 'uuid2']);
+  });
+  it('/api/chat/chats (GET) - unauthorized', () => {
+    return request(app.getHttpServer())
+      .get('/api/chat/chats')
+      .expect(500)
+      .expect({ statusCode: 500, message: 'Internal server error' });
   });
 
-  afterEach(async () => {
-    jest.useRealTimers();
-    await app.close();
+  it('/api/chat/name (GET)', () => {
+    return request(app.getHttpServer())
+      .post('/api/chat/create')
+      .send({ name: 'foo', visibility: 'PUBLIC', password: 'foo' })
+      .set('Content-Type', 'application/json')
+      .set('Authorization', bearer_value)
+      .expect(201)
+      .then((res) => {
+        return request(app.getHttpServer())
+          .get('/api/chat/name/' + res.body.chat_id)
+          .set('Authorization', bearer_value)
+          .expect(200)
+          .expect({});
+      });
+  });
+  it('/api/chat/name (GET) - unauthorized', () => {
+    return request(app.getHttpServer())
+      .post('/api/chat/create')
+      .send({ name: 'foo', visibility: 'PUBLIC', password: 'foo' })
+      .set('Content-Type', 'application/json')
+      .set('Authorization', bearer_value)
+      .expect(201)
+      .then((res) => {
+        return request(app.getHttpServer())
+          .get('/api/chat/name/' + res.body.chat_id)
+          .expect(500)
+          .expect({ statusCode: 500, message: 'Internal server error' });
+      });
   });
 });
