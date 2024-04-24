809c69f8a1ece3b17dc91534c88325534e4bfc5b
MyNameIsTrez <welfje@gmail.com>
2024-04-24 17:25:23 +0200

    Increase jwt expiration time to 30 days

--- containers/nestjs/src/auth/auth.module.ts
+++ containers/nestjs/src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { UsersModule } from '../users/users.module';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: '30d' },
     }),
   ],
   providers: [
