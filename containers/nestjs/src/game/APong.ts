8f379c2faacb87491cc4b93a4d12e727b3ef7694
Victor Bennekom <vbenneko@student.codam.nl>
2024-05-09 11:26:40 +0200

    Fixed issue with Velocity._dy not adjusting properly when _dx went to 25

--- containers/nestjs/src/game/APong.ts
+++ containers/nestjs/src/game/APong.ts
@@ -82,15 +82,15 @@ export class Velocity {
   }
   set len(value: number) {
     {
-      const fact = value / this.len;
+      let fact = value / this.len;
       // The if statement is put here to prevent the ball from gaining too much speed and going through the player paddle
       if (this._dx * fact <= 25) {
         this._dx *= fact;
         this._dy *= fact;
       } else {
-        const num: number = 25 / this._dx;
-        this._dx *= num;
-        this._dy;
+        fact = 25 / this._dx;
+        this._dx *= fact;
+        this._dy *= fact;
       }
     }
   }
