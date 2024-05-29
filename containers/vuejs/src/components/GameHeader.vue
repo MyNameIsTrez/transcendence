da08a8661ee22eb18507211d1d616e3d20370c5b
Milan Forsthövel <mforstho@student.codam.nl>
2024-05-29 16:07:00 +0200

    Added alert popup when trying to join the queue twice

--- containers/vuejs/src/components/GameHeader.vue
+++ containers/vuejs/src/components/GameHeader.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="game-header" v-if="!startOfGame">
+    <div role="alert" :class="`alert alert-warning w-96 ${alertVisibility}`">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="stroke-current shrink-0 h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+        />
+      </svg>
+      <span>Already in a lobby</span>
+    </div>
     <h1 class="game-title">{{ gameTitle }}</h1>
     <PlayButton v-if="!endOfGame && !queueing" @clicked="joinGame" :buttonText="'PLAY'" />
 
@@ -29,6 +45,8 @@ import { ref } from 'vue'
 
 const props = defineProps(['gameSocket'])
 const gameSocket = props.gameSocket
+
+const alertVisibility = ref('invisible')
 
 const emit = defineEmits(['resetCanvas'])
 const gameTitle = ref('PONG')
@@ -68,6 +86,10 @@ gameSocket.on('gameStart', () => {
 gameSocket.on('exception', (error) => {
   if (error.alreadyInALobby) {
     reset()
+    alertVisibility.value = 'visible'
+    setTimeout(() => {
+      alertVisibility.value = 'invisible'
+    }, 2500)
   }
 })
 </script>
