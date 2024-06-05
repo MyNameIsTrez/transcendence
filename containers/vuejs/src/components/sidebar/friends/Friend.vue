267fee14e4ce041aab84b390d06d5875ebf638e3
MyNameIsTrez <welfje@gmail.com>
2024-06-05 12:59:19 +0200

    Add profile button

--- containers/vuejs/src/components/sidebar/friends/Friend.vue
+++ containers/vuejs/src/components/sidebar/friends/Friend.vue
@@ -7,12 +7,19 @@
         </div>
       </figure>
     </router-link>
-    <div class="card-body w-64 h-28 grid justify-items-stretch">
-      <h2 class="text justify-self-start font-bold text-sm w-56">
+    <div class="card-body w-64 h-28">
+      <h2 class="text justify-self-start font-bold text-sm">
         <router-link :to="{ path: `user/${intraId}` }">{{ name }}</router-link>
       </h2>
-      <div class="btn m-1 btn-error w-8 h-8 min-h-0 justify-self-end" @click="removeFriend">
-        <span class="material-symbols-outlined" style="font-size: 20px">taunt</span>
+      <div class="card-actions justify-end">
+        <router-link :to="{ path: `user/${intraId}` }">
+          <button class="btn m-1 btn-success w-8 h-8 min-h-0">
+            <span class="material-symbols-outlined" style="font-size: 20px">account_circle</span>
+          </button>
+        </router-link>
+        <button class="btn m-1 btn-error w-8 h-8 min-h-0" @click="removeFriend">
+          <span class="material-symbols-outlined" style="font-size: 20px">taunt</span>
+        </button>
       </div>
     </div>
   </div>
