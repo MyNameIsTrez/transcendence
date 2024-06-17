ae84d314c2a9850dd30eded6d036f779dd4999aa
adfhjl <vbenneko@student.codam.nl>
2024-06-17 18:40:59 +0200

    Added a lot of TODO's in chat controller and new empty entrypoint

--- containers/nestjs/src/api/chat/chat.controller.ts
+++ containers/nestjs/src/api/chat/chat.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post, Request } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Request,
+} from '@nestjs/common';
 import { ChatService } from '../../chat/chat.service';
 import {
   IsInt,
@@ -89,31 +97,40 @@ export class ChatController {
     return await this.chatService.kickUser(dto.chat_id, req.user.intra_id);
   }
 
+  // TODO: Don't allow this if the chat is private and user is not in chat preferably
   @Get('name/:chat_id')
   public async name(@Param() dto: NameDto) {
     return await this.chatService.getName(dto.chat_id);
   }
 
+  // TODO: Only allow if user is in the chat
   @Get('history/:chat_id')
   public async history(@Param() dto: NameDto) {
     return await this.chatService.getHistory(dto.chat_id);
   }
 
+  // TODO: Preferably only allow if user is in the chat but not that important
   @Get('isAdmin/:chat_id/:intra_id')
   public async isAdmin(@Param() dto: OtherUserDto) {
     return await this.chatService.isAdmin(dto.chat_id, dto.intra_id);
   }
 
+  // TODO: Preferably only allow if user is in the chat but not that important
   @Get('isOwner/:chat_id/:intra_id')
   public async isOwner(@Param() dto: OtherUserDto) {
     return await this.chatService.isOwner(dto.chat_id, dto.intra_id);
   }
 
-  @Post('mute')
+  @Post('mute') // TODO: Replace with 'mute/:chat_id/:intra_id'
   public async mute(@Body() dto: MuteDto) {
     return await this.chatService.mute(dto.chat_id, dto.intra_id, dto.days);
   }
 
+  // TODO: Make sure only admin+ people are allowed to call this
+  //   @Post('mute/:chat_id/:intra_id')
+  //   public async mute(@Request() req, @Param() // TODO: Finish function definition)
+
+  // TODO: Preferably only allow if user is in the chat but not that important
   @Get('isMute/:chat_id/:intra_id')
   public async isMute(@Param() dto: OtherUserDto) {
     return await this.chatService.isMute(dto.chat_id, dto.intra_id);
@@ -124,11 +141,13 @@ export class ChatController {
     return await this.chatService.getInfo(dto.chat_id, req.user.intra_id);
   }
 
+  // TODO: Replace with 'edit/:chat_id'
   @Post('changePassword')
   public async changePassword(@Body() dto: PasswordDto) {
     return await this.chatService.changePassword(dto.chat_id, dto.password);
   }
 
+  // TODO: Replace with 'edit/:chat_id'
   @Post('changeVisibility')
   public async changeVisibility(@Body() dto: ChangeVisibilityDto) {
     return await this.chatService.changeVisibility(
@@ -137,6 +156,11 @@ export class ChatController {
       dto.password,
     );
   }
+
+  // TODO: Implement functionality
+  // TODO: Only allow owner to call this
+  @Post('edit/:chat_id')
+  public async edit(@Request() requestAnimationFrame) {}
 
   @Get('leave/:chat_id')
   public async leave(@Request() req, @Param() dto: NameDto) {
